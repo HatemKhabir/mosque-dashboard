5e2fbbd3d1bee10c66436fd39fc8ade877a98a66
HatemKhabir <hatemkhabir.cse@gmail.com>
2025-08-12 17:12:27 +0200

    Backend Initialized

---
+++ src/main/java/com/hatemkhabir/mosque_dashboards/MosqueDashboardsApplication.java
@@ -0,0 +1,13 @@
+package com.hatemkhabir.mosque_dashboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosqueDashboardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosqueDashboardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hatemkhabir/mosque_dashboards/MosqueDashboardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatemkhabir.mosque_dashboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosqueDashboardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
