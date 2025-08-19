dbb3dd046252450c8f0bb3e5e0f1604d596dbb85
HatemKhabir <hatemkhabir.cse@gmail.com>
2025-08-19 15:09:15 +0200

    Implemented File Services with cloudinary

--- src/main/java/com/hatemkhabir/mosque_dashboards/service/FileService.java
+++ src/main/java/com/hatemkhabir/mosque_dashboards/service/FileService.java
@@ -9,5 +9,67 @@ generateDownloadUrl(fileId) - Create temporary access URL
 validateFileType(file, allowedTypes) - Check file format
 extractMetadata(file) - Get duration, page count, etc.
 * */
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class FileService {
+
+    private final Cloudinary cloudinary;
+
+    public String storeFile(MultipartFile file, String folder) {
+        try {
+            Map options = ObjectUtils.asMap(
+                    "folder", folder,
+                    "resource_type", "auto"
+            );
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            log.error("File failed to upload with error {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String deleteFile(String fileId){
+        try {
+           Map uploadResult = cloudinary.uploader().destroy(fileId, ObjectUtils.emptyMap());
+            return uploadResult.get("result").toString();
+        }catch (Exception e){
+            log.error("File deleting error : {}",e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteFiles(String[] fileIds){
+        try {
+            for (String fileId : fileIds) {
+            cloudinary.uploader().destroy(fileId,ObjectUtils.emptyMap());
+            };
+            return true;
+        }catch (Exception e){
+            log.error("Files deleting error : {}",e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateDownloadUrl(String publicId) {
+        return cloudinary.url()
+                .generate(publicId);
+    }
+
+
+
+
 }
